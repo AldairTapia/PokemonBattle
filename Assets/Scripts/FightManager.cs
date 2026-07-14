f0c02b07c9a8b45e5204ca33b549a1209c16ef36
LionCking700 <Hellckingder2000@gmail.com>
2026-07-13 20:14:32 -0600

    13jul

--- Assets/Scripts/FightManager.cs
+++ Assets/Scripts/FightManager.cs
@@ -92,8 +92,12 @@ public class FightManager : MonoBehaviour
             {
                 SoundManager.instance.Play(defender.FighterData.deadSoundNmae);
                 RemoveFighter(defender);
-                DialogSystem.Instance.ShowDialog(attacker.FighterData.fighterName + " wins the fight!");
-                FighterWin(attacker);
+                if (fighters.Count == 1)
+                {
+                  DialogSystem.Instance.ShowDialog(attacker.FighterData.fighterName + " wins the fight!");
+                  FighterWin(attacker);  
+                }
+                
             }
             yield return new WaitForSeconds(1.5f);
         }
